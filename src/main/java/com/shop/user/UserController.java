73efa09243a571711444821baa86d485e8e02b0b
haideratish2689@gmail.com <haideratish2689@gmail.com>
2022-06-15 01:58:53 +0500

    working on admin page

--- src/main/java/com/shop/user/UserController.java
+++ src/main/java/com/shop/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -38,5 +39,18 @@ public class UserController {
         userService.save(user);
         redirectAttributes.addFlashAttribute("message", "User is added successfully.");
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/edit/{edit}")
+    public String editUser(@PathVariable(name = "id")Integer id, RedirectAttributes redirectAttributes,Model model){
+        try {
+            User user = userService.get(id);
+            model.addAttribute("user",user);
+            return "user_form";
+        }catch (UserNotFoundException ex){
+            redirectAttributes.addFlashAttribute("message", ex.getMessage());
+            return "redirect:/users";
+        }
+
     }
 }
--- src/main/java/com/shop/user/UserService.java
+++ src/main/java/com/shop/user/UserService.java
@@ -3,10 +3,12 @@ package com.shop.user;
 import com.shop.entity.Role;
 import com.shop.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -43,5 +45,14 @@ public class UserService {
     public boolean isEmailUnique(String email){
         User userByEmail = userRepo.getUserByEmail(email);
         return userByEmail == null;
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+        try{
+            return userRepo.findById(id).get();
+        }catch (NoSuchElementException ex){
+            throw new UserNotFoundException("user not found with id:"+id);
+        }
+
     }
 }
